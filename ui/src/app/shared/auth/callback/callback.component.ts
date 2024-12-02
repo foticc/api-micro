7386234c7ccfb0ceb638e24cfc598456b4e535cd
foticc <foticc@163.com>
2024-12-02 14:06:06 +0800

    // add oauth2

--- ui/src/app/shared/auth/callback/callback.component.ts
+++ ui/src/app/shared/auth/callback/callback.component.ts
@@ -1,11 +1,11 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import { finalize } from 'rxjs/operators';
 
 import { AuthOauth2Service } from '@core/services/common/auth.oauth2.service';
 import { LoginInOutService } from '@core/services/common/login-in-out.service';
-import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { SpinService } from '@store/common-store/spin.service';
 
 @Component({
   standalone: true,
@@ -16,38 +16,49 @@ export class CallbackComponent implements OnInit {
   private loginInOutService = inject(LoginInOutService);
   private router: Router = inject(Router);
   private route: ActivatedRoute = inject(ActivatedRoute);
+  private spinService = inject(SpinService);
   destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
+    // 设置全局loading
     this.route.queryParams.subscribe(params => {
       if (params && params['code'] && params['state']) {
         this.tryLogin();
       } else {
-        this.router.navigateByUrl('default/dashboard/analysis');
+        this.router.navigateByUrl('/default/dashboard/analysis');
       }
     });
   }
 
   tryLogin(): void {
+    this.spinService.setCurrentGlobalSpinStore(true);
     this.auth2Service
       .tryLogin()
-      .pipe(takeUntilDestroyed(this.destroyRef))
+      .pipe(
+        // 无论如何设置全局loading为false
+        finalize(() => {
+          this.spinService.setCurrentGlobalSpinStore(false);
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
       .subscribe({
         next: res => {
+          this.spinService.setCurrentGlobalSpinStore(false);
           if (res) {
             let token = this.auth2Service.accessToken();
             if (token) {
               this.loginInOutService.loginIn(token).then(() => {
-                this.router.navigateByUrl('default/dashboard/analysis');
+                this.router.navigateByUrl('/default/dashboard/analysis');
               });
             }
           } else {
-            this.router.navigateByUrl('login/login-form');
+            this.router.navigateByUrl('/login/login-form');
           }
         },
         error: err => {
-          this.router.navigateByUrl('login/login-form');
+          this.router.navigateByUrl('/login/login-form');
         }
       });
   }
+
 }
