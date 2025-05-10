7a4cd6c3ec88fab0a24230ffa29a0d53ee6f955e
foticc <foticc@163.com>
2025-05-11 00:01:55 +0800

    demo page

--- ui/src/app/app-routing.ts
+++ ui/src/app/app-routing.ts
@@ -1,11 +1,9 @@
 import { Route } from '@angular/router';
 
-import { CallbackComponent } from '@shared/auth/callback/callback.component';
 
 export const appRoutes = [
   { path: '', redirectTo: '/default', pathMatch: 'full' },
   { path: 'blank', loadChildren: () => import('./layout/blank/blank-routing') },
-  { path: 'callback', component: CallbackComponent },
   { path: 'login', data: { preload: true }, loadChildren: () => import('./pages/login/login-routing') },
   { path: 'default', data: { preload: true }, loadChildren: () => import('./layout/default/default-routing') },
   {
