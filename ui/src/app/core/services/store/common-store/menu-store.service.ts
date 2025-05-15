918bcc2ba57863e828e3d515cb50b20d309aff99
foticc <foticc@163.com>
2025-05-15 19:38:02 +0800

    // page param type

--- ui/src/app/core/services/store/common-store/menu-store.service.ts
+++ ui/src/app/core/services/store/common-store/menu-store.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+import { CacheService } from '@core/services/common/cache.service';
 import { Menu } from '@core/services/types';
 
 // 菜单store service
@@ -9,12 +10,27 @@ import { Menu } from '@core/services/types';
 })
 export class MenuStoreService {
   private menuArray$ = new BehaviorSubject<Menu[]>([]);
+  private cacheService = inject(CacheService);
+
+  constructor() {
+    const menus = this.cacheService.get<Menu[]>('menus');
+    if (menus) {
+      this.setMenuArrayStore(menus);
+    }
+  }
 
   setMenuArrayStore(menuArray: Menu[]): void {
+    this.cacheService.set<Menu[]>('menus', menuArray);
     this.menuArray$.next(menuArray);
   }
 
   getMenuArrayStore(): Observable<Menu[]> {
+    if (this.menuArray$.value.length === 0) {
+      const cachedData = this.cacheService.get<Menu[]>('menus');
+      if (cachedData) {
+        this.menuArray$.next(cachedData);
+      }
+    }
     return this.menuArray$.asObservable();
   }
 }
