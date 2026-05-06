d42fc274ff9d9feb022c986983776ed19f4d7d87
hujian123 <287643967@qq.com>
2026-05-06 16:04:56 +0800

    refactor: judgeAuth改成函数式

--- ui/src/app/core/services/common/guard/judgeAuth.guard.ts
+++ ui/src/app/core/services/common/guard/judgeAuth.guard.ts
@@ -1,6 +1,5 @@
-import { computed, inject, Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivateChildFn } from '@angular/router';
-import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateChildFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 
 import { LoginInOutService } from '@core/services/common/login-in-out.service';
 import { MenuStoreService } from '@store/common-store/menu-store.service';
@@ -14,66 +13,51 @@ import { Menu } from '../../types';
 // 有兴趣的可以看看class与fn的争议https://github.com/angular/angular/pull/47924
 // 我这里提供了跟judgeLogin.guard.ts的不同写法，供大家参考,也可以去官网查找mapToCanActivate 这个api，
 // 用于切换路由时判断该用户是否有权限进入该业务页面，如果没有权限则跳转到登录页
-@Injectable({
-  providedIn: 'root'
-})
-export class JudgeAuthGuardService {
-  selMenu: Menu | null = null;
-  loginOutService = inject(LoginInOutService);
-  router = inject(Router);
-  userInfoService = inject(UserInfoStoreService);
-  menuStoreService = inject(MenuStoreService);
-  message = inject(NzMessageService);
-  authCodeArray = computed(() => this.userInfoService.$userInfo().authCode);
-  menuNavList = computed(() => this.menuStoreService.$menuArray());
 
-  // 保存当前的menu到this.selMenu
-  getMenu(menu: Menu[], url: string): void {
-    // eslint-disable-next-line @typescript-eslint/prefer-for-of
-    for (let i = 0; i < menu.length; i++) {
-      if (url === menu[i].path) {
-        this.selMenu = menu[i];
-        return;
-      } else {
-        if (menu[i].children && menu[i].children!.length > 0) {
-          this.getMenu(menu[i].children!, url);
-        }
-      }
+function findMenu(menu: Menu[], url: string): Menu | null {
+  for (const item of menu) {
+    if (url === item.path) return item;
+    if (item.children && item.children.length > 0) {
+      const found = findMenu(item.children, url);
+      if (found) return found;
     }
   }
+  return null;
+}
 
-  getResult(code: string, authCodeArray: string[]): boolean | UrlTree {
-    if (authCodeArray.includes(code)) {
-      return true;
-    } else {
-      this.message.error('您没有权限登录该模块');
-      this.loginOutService.loginOut();
-      return this.router.parseUrl('/login');
-    }
-  }
-
-  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    while (route.firstChild) {
-      route = route.firstChild;
-    }
-    // 如果有authCode，则表示是页面上点击按钮跳转到新的路由，而不是菜单中的路由
-    if (route.data['authCode']) {
-      return this.getResult(route.data['authCode'], this.authCodeArray());
-    }
-
-    // 如果是菜单上的按钮，则走下面
-    this.getMenu(this.menuNavList(), state.url);
-    // 没找到菜单，直接回登录页
-    if (!this.selMenu) {
-      return this.getResult(fnGetUUID(), this.authCodeArray());
-    }
-    const selMenuCode = this.selMenu.code;
-    this.selMenu = null;
-    // 找到了菜单，但是菜单的权限码用户不拥有，则跳转到登录页
-    return this.getResult(selMenuCode!, this.authCodeArray());
-  }
+function makeGetResult(authCodeArray: string[], router: Router, loginOutService: LoginInOutService, message: NzMessageService) {
+  return (code: string): boolean | UrlTree => {
+    if (authCodeArray.includes(code)) return true;
+    message.error('您没有权限登录该模块');
+    loginOutService.loginOut();
+    return router.parseUrl('/login');
+  };
 }
 
 export const JudgeAuthGuard: CanActivateChildFn = (childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  return inject(JudgeAuthGuardService).canActivateChild(childRoute, state);
+  const getResult = makeGetResult(
+    inject(UserInfoStoreService).$userInfo().authCode,
+    inject(Router),
+    inject(LoginInOutService),
+    inject(NzMessageService)
+  );
+  const menuNavList = inject(MenuStoreService).$menuArray();
+
+  while (childRoute.firstChild) {
+    childRoute = childRoute.firstChild;
+  }
+
+  // 如果有authCode，则表示是页面上点击按钮跳转到新的路由，而不是菜单中的路由
+  if (childRoute.data['authCode']) {
+    return getResult(childRoute.data['authCode']);
+  }
+
+  // 如果是菜单上的按钮，则走下面
+  const selMenu = findMenu(menuNavList, state.url);
+  // 没找到菜单，直接回登录页
+  if (!selMenu) {
+    return getResult(fnGetUUID());
+  }
+  // 找到了菜单，但是菜单的权限码用户不拥有，则跳转到登录页
+  return getResult(selMenu.code!);
 };
