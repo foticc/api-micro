1e067389fa317aeb1e59e6289d08c920adda92da
hujian123 <287643967@qq.com>
2026-05-18 08:57:35 +0800

    refactor: login 样式微调

--- ui/src/app/pages/login/login.component.ts
+++ ui/src/app/pages/login/login.component.ts
@@ -7,13 +7,14 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDropdownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzTypographyComponent } from 'ng-zorro-antd/typography';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NzIconModule, NzButtonModule, NzDropdownModule, NzMenuModule, TranslateModule, RouterOutlet]
+  imports: [NzIconModule, NzButtonModule, NzDropdownModule, NzMenuModule, TranslateModule, RouterOutlet, NzTypographyComponent]
 })
 export class LoginComponent {
   private languageService = inject(LanguageService);
